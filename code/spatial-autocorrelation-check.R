a9352f99cd828084ccd3801bffe9a8e6bae691d3
Stephen Wood <stephenawood@gmail.com>
2018-06-01 09:02:46 -0700

    Add temporal and spatial autocorrelation checks

--- code/spatial-autocorrelation-check.R
+++ code/spatial-autocorrelation-check.R
@@ -1,25 +1,81 @@
 
-# Load libraries
+# LOAD LIBRARIES
 library(tidyverse)
-library(tigris)
+library(spdep)
+library(broom)
 library(spacetime)
 
-# Load data
-counties <- tigris::counties("Illinois", cb = TRUE)
-load('data-subset/IL_toy_data_05112018.RData')
+# LOAD DATA
+counties <- tigris::counties(year = 2017)
+load('data/yield_data_temp_05302018.RData')
 
-# Subset original data frame for the counties that have data in time series
-counties.new <- subset(counties, NAME %in% unique(d.IL$NAME))
+# MANIPULATE DATA
+## Subset original data frame for the counties that have data in time series
+sub.counties <- subset(counties, GEOID %in% unique(d$FIPS_county))
 
-# Turn temporal data into temporal variable
-d.IL$Year <- as.POSIXct(paste(d.IL$Year, "-01-01", sep=""), tz = "GMT")
-d.IL <- d.IL[order(d.IL['Year']),]
+# SPATIAL AUTOCORRELATION CHECKING
+## Regress yield against year for each county
+model <- d %>% 
+  as.tibble() %>%
+  select(FIPS_county,Year,Yield.bu.acre) %>%
+  nest(-FIPS_county) %>%
+  mutate(fit = map(data, ~ median(predict(loess(Yield.bu.acre ~ Year, span = 0.91, data = .)))))
 
-# Remove non temporal data
-IL.time <- select(d.IL,NAME,Year,Yield.bu.acre)
-IL.space.time <- gather(IL.time,measurement=Yield.bu.acre)
-names(IL.space.time)[1] <- 'County'
+## Collapse to county-level yield score
+c.data <- select(d,FIPS_county,Year,Yield.bu.acre) %>% 
+  spread(Year,Yield.bu.acre) %>% 
+  cbind(unlist(model$fit)) %>%
+  select(FIPS_county,`unlist(model$fit)`)
+names(c.data)[2] <- 'median.yield'
 
-# Create actual time variable
-spatial.counties <- STFDF(counties.new, unique(d.IL$Year),data=IL.space.time)
+## Merge with spatial data frame and drop NA values
+combined <- sp::merge(sub.counties,c.data,by.x="GEOID10",by.y="FIPS_county") %>%
+  subset(is.na(median.yield)==FALSE)
+
+## Plot county median yield
+brks <- quantile(combined$median.yield, seq(0,1,1/7))
+cols <- grey((length(brks):2)/length(brks))
+dens <- (2:length(brks))*3
+maps::map('usa')
+plot(combined, col=cols[findInterval(combined$median.yield, brks, all.inside=TRUE)] , add = T)
+
+## Define who is neighbors with who
+## Do this with basic approach and nearest neighbors with a fairly flexible k
+w1 <- combined %>% poly2nb() %>% nb2listw(style="W",zero.policy=TRUE) # Even with zero.policy=T I get NAs
+IDs <- row.names(as(combined, "data.frame"))
+coords <- sp::coordinates(combined)
+w2 <- knn2nb(knearneigh(coords, k = 4), row.names = IDs) %>% 
+        nb2listw(style='B')
+
+## Compute spatially lagged yield values
+yield.lag <- lag.listw(w2, combined$median.yield)
+
+## Use Moran's I to check for correlations
+moran.test(combined$median.yield,w2)
+moran.mc(combined$median.yield, w2, nsim=599) %>%
+  plot()
+
+
+
+
+
+
+
+
+
+# # Create spatio-temporal object
+# # Subset original data frame for the counties that have data in time series
+# counties.new <- subset(counties, NAME %in% unique(d.IL$NAME))
+# 
+# # Turn temporal data into temporal variable
+# d.IL$Year <- as.POSIXct(paste(d.IL$Year, "-01-01", sep=""), tz = "GMT")
+# d.IL <- d.IL[order(d.IL['Year']),]
+# 
+# # Remove non temporal data
+# IL.time <- select(d.IL,NAME,Year,Yield.bu.acre)
+# IL.space.time <- gather(IL.time,measurement=Yield.bu.acre)
+# names(IL.space.time)[1] <- 'County'
+# 
+# # Create actual time variable
+# spatial.counties <- STFDF(counties.new, unique(d.IL$Year),data=IL.space.time)
 
--- code/temporal-autocorrelation-check.R
+++ code/temporal-autocorrelation-check.R
@@ -4,74 +4,52 @@
 # Last update: May 31, 2018                   #
 ###############################################
 
-# Load libraries
+# LOAD LIBRARIES
 library(tidyverse)
 library(itsadug)    # For checking temporal autocorrelation
 library(mgcv)       # For mixed GAM modeling
 
-# Load data
-load('data-subset/IL_toy_data_05112018.RData')
+# LOAD DATA
+load('data/yield_data_temp_05302018.RData')
 
-# Plot all yield through time
-ggplot(d.IL,aes(y=Yield.bu.acre,x=Year)) + geom_point(cex=0.5) + theme_bw()
+# PLOT DATA THROUGH TIME
+ggplot(d,aes(y=Yield.bu.acre,x=Year)) + geom_point(cex=0.5) + theme_bw()
 
-# Plot acres harvested through time
-ggplot(d.IL,aes(y=Acres.harvested,x=Year)) + geom_point(cex=0.5) + theme_bw()
+# CHECK TEMPORAL AUTOCORRELATION
+## Aggregate all counties to one yield value per year and plot through time
+d.agr <- d[,c('Year','Acres.harvested','Yield.bu.acre')] 
+d.agr <- aggregate(. ~ Year, median, data=d.agr)
+ggplot(d.agr,aes(y=Yield.bu.acre,x=Year)) + geom_point() + theme_bw()
 
-# Aggregate counties to one yield value and plot through time
-d.IL.agr <- d.IL[,c('Year','Acres.harvested','Yield.bu.acre')] 
-d.IL.agr <- aggregate(. ~ Year, mean, data=d.IL.agr)
-ggplot(d.IL.agr,aes(y=Yield.bu.acre,x=Year)) + geom_point() + theme_bw()
-ggplot(d.IL.agr,aes(y=Acres.harvested,x=Year)) + geom_point() + theme_bw()
-
-# Check for temporal autocorrelation in both yield and acres harvested
-d.IL.agr <- arrange(d.IL.agr, desc(-Year))
-d <- d.IL.agr$Yield.bu.acre
-a <- d[-length(d)]
-b <- d[-1]
+## Check for temporal autocorrelation in yield
+d.agr <- arrange(d.agr, desc(-Year))
+d.new <- d.agr$Yield.bu.acre
+a <- d.new[-length(d.new)]
+b <- d.new[-1]
 plot(a,b,xlab="t",ylab="t-1")
-cor(a,b) #0.33
-acf(d)
+cor(a,b) #0.51
+acf(d.new)
 
-d2 <- d.IL.agr$Acres.harvested
-a <- d2[-length(d2)]
-b <- d2[-1]
-plot(a,b,xlab="t",ylab="t-1")
-cor(a,b) #0.65
-acf(d2)
-
-# Checking for temporal autocorrelation without collapsing data
-lm1 <- lm(Yield.bu.acre ~ as.factor(Year) + NAME, data=t.dat)
-lm2 <- lm(Acres.harvested ~ Year + NAME,data=t.dat)
-
+## Check for temporal autocorrelation without collapsing data
+lm1 <- lm(Yield.bu.acre ~ as.factor(Year) + as.factor(FIPS_county), data=d)
 plot(lm1)
-plot(lm2)
-
 acf(resid(lm1))
-acf(resid(lm2))
 
-influence.measures(lm1)
+## Compare time series to non time series
+m1 <- bam(Yield.bu.acre ~ as.factor(Year)+as.factor(FIPS_county), data=d)
 
-## Looks like acres harvested has temporal errors, but not yield
-## Lets look some more
-m1 <- bam(Acres.harvested ~ as.factor(Year)+NAME, data=t.dat)
-
-par(mfrow=c(1,3), cex=1.1)
-# default ACF function:
 acf(resid(m1), main="acf(resid(m1))")
-# resid_gam:
 acf(resid_gam(m1), main="acf(resid_gam(m1))")
-# acf_resid:
 acf_resid(m1, main="acf_resid(m1)")
 
-par(mfrow=c(1,1), cex=1.1)
+### Extract first lag in time series and assign it to A1 correlation parameter
 r1 <- start_value_rho(m1, plot=TRUE)
 acf(resid(m1), plot=FALSE)$acf[2]
 
-m1AR1 <- bam(Acres.harvested ~ as.factor(Year)+NAME, rho = r1, data=t.dat, AR.start=t.dat$start.event)
-par(mfrow=c(1,2), cex=1.1)
+## Generate specific time series
+simdat <- start_event(as.data.frame(d), column="Year", event="FIPS_county")
+
+## Fit time series
+m1AR1 <- bam(Yield.bu.acre ~ as.factor(Year)+as.factor(FIPS_county), rho = r1, data=simdat, AR.start=simdat$start.event)
 acf_resid(m1)
 acf_resid(m1AR1)
-
-
-plot(ts(d.IL,start=c(1997,1),fr=20))
