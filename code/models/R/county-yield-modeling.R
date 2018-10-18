8648af6f7ad135bab687cb3ad832e76d7d1ad141
Stephen Wood <stephenawood@gmail.com>
2018-10-18 06:32:32 -0700

    County-scale yield data modeling

--- code/models/R/county-yield-modeling.R
+++ code/models/R/county-yield-modeling.R
@@ -5,13 +5,13 @@
 ###################
 
 library(tidyverse)
+library(lme4)
 
 #### READ IN DATA ####
-
 # Drought data
-load("data/drought_by_county.RData")
-drought <- data_by_county_2
-rm(data_by_county_2)
+load("data/weather/DSCI_county_by_year.RData")
+drought <- drought.summary
+rm(drought.summary)
 
 # Yield data
 load('data/yield_data_temp_05302018.RData')
@@ -24,7 +24,43 @@ soil <- readRDS("/home/shares/soilcarbon/soilc-midwest/data/soil/county-soilgrid
 
 # Spatial data
 counties <- tigris::counties(year = 2017)
-sub.counties <- subset(counties, GEOID %in% unique(d$FIPS_county))
+sub.counties <- subset(counties, GEOID %in% unique(yield$FIPS_county))
+
+
+#### MANIPULATE DATA ####
+names(yield)[1] <- "GEOID"
+names(drought)[2] <- "GEOID"
+
+# Merge all data frames
+yield %>%
+  select(GEOID:FIPS,Acres.harvested:Yield.bu.acre) %>%
+  left_join(soil, by = "GEOID") %>%
+  select(GEOID:Yield.bu.acre,ALAND:Order) %>%
+  left_join(drought, by="GEOID") %>%
+  select(GEOID:Order,DSCI.mean:DSCI.mode) -> all_data
 
 
 #### FIT MODEL ####
+all_data$Year <- as.factor(all_data$Year.x)
+all_data$GEOID <- as.factor(all_data$GEOID)
+all_data$H <- exp(all_data$ph_h2o_M_sl4_100m)
+all_data$H.sq <- all_data$H^2
+
+#### Are yields lower under drought years ####
+pairs(all_data[,c(8,13:16,19:21)])
+# Basic model
+plot(Yield.bu.acre~DSCI.median, data=all_data)
+lm(Yield.bu.acre ~ DSCI.mean, data=all_data) %>%
+  summary()
+lmer(Yield.bu.acre ~ 
+     DSCI.mean + soc_M_sl4_100m + soc_M_sl4_100m:DSCI.mean + H + sand_M_sl4_100m + 
+       (1|GEOID) + (1|Order), 
+   data=all_data) %>% 
+  summary()
+
+# Spatial model
+
+# Temporal model
+
+
+
